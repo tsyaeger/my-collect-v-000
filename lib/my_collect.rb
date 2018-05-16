16a6a05f384426e54eb685f2f894e3d1b7a19488
taryn yaeger <tsyaeger@gmail.com>
2018-05-16 01:37:01 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,21 @@
 
 
+def my_collect(collection)
+  
+  if block_given?
+    counter = 0
+    while counter < collection.length 
+      puts collection[counter]
+    end
+    
+    
+    
+  else
+    
+  end
+  
+  
+end
+
+
+
