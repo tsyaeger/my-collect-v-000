c2159a64bd001263111dc91238d4325b6e774adb
taryn yaeger <tsyaeger@gmail.com>
2018-05-16 01:45:30 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,19 @@
 
 
+def my_collect(collection)
+  
+  if block_given?
+    counter = 0
+    new_arr = []
+    while counter < collection.length 
+      new_arr << yield(collection[counter])
+      counter += 1
+    end
+    new_arr
+  else
+    "empty"
+    
+  end
+end
+
+my_collect(['Tim Jones', 'Tom Smith', 'Sophie Johnson', 'Antoin Miller']) {|item| item}
